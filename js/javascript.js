751707e403c1cb5a36bb94db87d8ece54763e488
DMU-P2587267 <72441357+DMU-P2587267@users.noreply.github.com>
2023-04-15 14:47:47 +0100

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+function toggleFlip(card) {
+    card.classList.toggle('flipped');
+  }
+/* The function says that the element(s) with the class 'card' will be flipped when it gets toggled.
+   By the term toggled it means that it removes an element and adds another in its place and can them return it to the first element.
+   In this case the element with class gets changed with the element that has toggled as a class on it. */  
